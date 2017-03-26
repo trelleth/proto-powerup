905a0942c9ee318c8b3f900cc8fba7655b2e1087
Stefaan Ponnet <stefaan@ponnet.com>
2017-03-26 14:01:53 +0200

    Moved contract stuff behind ‘Trelleth ICO’ button

--- frontend/js/overlay.js
+++ frontend/js/overlay.js
@@ -6,7 +6,8 @@ var t = TrelloPowerUp.iframe();
 var num = t.arg('rand');
 
 t.render(function(){
-  console.log('web3', window.web3.isConnected());
+  //debugger;
+  //console.log('web3', window.web3.isConnected());
   // this function we be called once on initial load
   // and then called each time something changes that
   // you might want to react to, such as new data being
@@ -14,7 +15,30 @@ t.render(function(){
 });
 
 document.getElementById('icobtn').addEventListener('click', function(){
-  console.log('icobtn clickced', t.iframe.web3.currentProvider());
+  //console.log('icobtn clickced', t.iframe.web3.currentProvider());
+
+
+var contractAbi = [{"constant":false,"inputs":[{"name":"_trelloCardId","type":"string"},{"name":"_matchAmount","type":"uint256"}],"name":"trellethIt","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"}];
+var contractAddress = "0x73caed002843d980889538bf1a5dd22a4ed5e633";
+
+debugger;
+//var web3 = window.parent.web3;
+//var web3 = new Web3(new Web3.providers.HttpProvider("https://metamask.io"));
+console.log('web3? ', web3.isConnected());
+//;
+ var myContract = web3.eth.contract(contractAbi)
+ var myContractinstance = myContract.at(contractAddress);
+
+// myContractinstance.trellethIt.sendTransaction("trellocardjeId", 10, function(err, res){
+//   console.log(err, res);
+// });
+
+myContractinstance.trellethIt.sendTransaction(contractAddress, 10000000000000000, "trllocardid", 10, function(err, res){
+  console.log(err, res);
+});
+
+
+
 });
 
 // close overlay if user clicks outside our content
